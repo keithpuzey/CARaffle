2d8c990459afff2138dd102e2eaa62c494fda7f0
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-04 17:16:17 +0100

    Original

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<div id="layout" class="pure-g">
+		<div class="sidebar pure-u-1 pure-u-md-1-4">
+			<div class="header">
+				<h1 class="brand-title">CA WebStore</h1>
+				<h2 class="brand-tagline">For all your everyday needs</h2>
+
+				<nav class="nav">
+					<ul class="nav-list">
+						<li class="nav-item">
+							<a class="pure-button" href="index.php">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="pure-button" href="about.php">About</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
