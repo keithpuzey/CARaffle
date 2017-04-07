879b0196aae2d7aaf478ba24bed59e2efe4cbf3f
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-07 09:05:04 +0100

    Modify Base code

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,11 +1,7 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://caraffle3.us-east-1.elasticbeanstalk.com/',
   specs: ['spec.js'],
-    multiCapabilities: [{browserName: 'chrome'}, {browserName: 'firefox'}, {tunnel-identifier: process.env.TRAVIS_JOB_NUMBER}, {build: 52},
-{name: 'CA Raffle Test'}]  
-  
-  onPrepare: function(){
+    onPrepare: function(){
     browser.driver.get('http://caraffle3.us-east-1.elasticbeanstalk.com/');
     element(by.linkText('About')).click();
     element(by.linkText('Home')).click();
