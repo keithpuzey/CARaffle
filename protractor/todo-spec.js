391b1ce2fc69a4de5986189798498da7d26c06f3
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-07 11:37:12 +0100

    Add Click for About Screen

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -2,6 +2,6 @@ describe('CA Raffle UI Test', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://preprod.cacdsolutions.com/index.php');
-    element(by.id('Name')).sendKeys('SauceLabs Test');
+    element(by.linkText('About')).click();
     });
 });
