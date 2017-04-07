b40479a921bf13e0c3329f94858a552540b79aed
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-07 09:20:02 +0100

    Original

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,13 +1,4 @@
 exports.config = {
-  baseUrl: 'http://caraffle3.us-east-1.elasticbeanstalk.com/',
-  specs: ['spec.js'],
-    onPrepare: function(){
-    browser.driver.get('http://caraffle3.us-east-1.elasticbeanstalk.com/');
-    element(by.linkText('About')).click();
-    element(by.linkText('Home')).click();
-    element(by.linkText('About')).click();
-    element(by.linkText('Home')).click();
-    element(by.css('[name="Name"]')).sendKeys('Keith 123');
-    element(by.buttonText('Enter Raffle')).click();
-  }
-}
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['CARaffle.js']
+};
