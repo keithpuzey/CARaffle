dd0bd4cd14238ade69aee2f60fa512c72a4ef55a
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-07 09:52:49 +0100

    Original

--- protractor/CARaffle.js
+++ protractor/CARaffle.js
@@ -1,13 +1,13 @@
 exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://caraffle3.us-east-1.elasticbeanstalk.com/',
   specs: ['spec.js'],
-    onPrepare: function(){
+  multiCapabilities: [{browserName: 'firefox'}, ],
+  onPrepare: function(){
     browser.driver.get('http://caraffle3.us-east-1.elasticbeanstalk.com/');
     element(by.linkText('About')).click();
     element(by.linkText('Home')).click();
-    element(by.linkText('About')).click();
-    element(by.linkText('Home')).click();
-    element(by.css('[name="Name"]')).sendKeys('Keith 123');
+    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Enter Raffle')).click();
   }
 }
