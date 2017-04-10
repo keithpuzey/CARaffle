8da50cf954a90614bd75358186eeebc79388c1c2
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-10 17:30:23 +0100

    change layout

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,13 @@
 <div id="layout" class="pure-g">
 		<div class="sidebar pure-u-1 pure-u-md-1-4">
-			<IMG SRC="/images/raffle.gif" ALT="Raffle" WIDTH=140 HEIGHT=140>
-							
-			<div class="header">
+			<IMG SRC="/images/raffle.gif" ALT="Raffle" WIDTH=200 HEIGHT=200>
+				<div class="header">
 				<h1 class="brand-title">CA Kick Off</h1>
+				<br>
 				<h2 class="brand-tagline">Enter to Win</h2>
+				<br>
 				<h2 class="brand-tagline">Build Number $BUILD$ </h2>
-
+    <br>
 				<nav class="nav">
 					<ul class="nav-list">
 						<li class="nav-item">
