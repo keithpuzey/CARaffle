c477232854fef60ea0fe1e1b0ed0d2cbe0699c41
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-10 10:00:00 +0100

    Tokenised

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,9 @@
 <div id="layout" class="pure-g">
 		<div class="sidebar pure-u-1 pure-u-md-1-4">
 			<div class="header">
-				<h1 class="brand-title">CA WebStore</h1>
-				<h2 class="brand-tagline">For all your everyday needs</h2>
+				<h1 class="brand-title">CA Kick Off</h1>
+				<h2 class="brand-tagline">Enter to Win</h2>
+				<h2 class="brand-tagline">Build Number $BUILD$ </h2>
 
 				<nav class="nav">
 					<ul class="nav-list">
