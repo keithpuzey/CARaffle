b5faa6219117cae375a603ea550a5e23026559c6
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-10 09:59:03 +0100

    Original

---
+++ about2.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+		<link rel="stylesheet" href="css/layouts/store.css">
+		<title></title>
+
+	</head>
+
+	<body>
+		<?php include('includes/adminheader.php') ?>
+
+		<div class="content pure-u-1 pure-u-md-3-4">
+			<h1 class="brand-title">Game Rules</h1> 
+			
+			<div style="padding-top:20px;">
+			<b>Rules of the Game.</b>
+			</div>
+		</div>			
+		
+		<?php include('includes/footer.php') ?>
+	</body>	
+</html>
