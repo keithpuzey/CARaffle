5835e908731d33801c94ff79ef56ed728ed02069
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-10 10:07:18 +0100

    Original

---
+++ includes/adminheader.php
@@ -0,0 +1,19 @@
+<div id="layout" class="pure-g">
+		<div class="sidebar pure-u-1 pure-u-md-1-4">
+			<div class="header">
+				<h1 class="brand-title">CA Kick Off</h1>
+				<h2 class="brand-tagline">Enter to Win</h2>
+				<h2 class="brand-tagline">Build Number %BUILD$ </h2>
+
+				<nav class="nav">
+					<ul class="nav-list">
+						<li class="nav-item">
+							<a class="pure-button" href="index2.php">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="pure-button" href="about2.php">About</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
