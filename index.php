14ae33bcabd9bd852870a6eba0f35ee117ba8a4a
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-11 09:37:51 +0100

    add graphics

--- index.php
+++ index.php
@@ -12,19 +12,20 @@
 	<body>
 	<?php include('includes/header.php') ?>
 		<div class="content pure-u-1 pure-u-md-3-4">
-			<h1 class="brand-title">CA virtual raffle</h1>
+		<h1 class="brand-title">CA virtual raffle</h1>
         <br>
         <br>
 
-						<div class="pure-controls">
+	<div class="pure-controls">
              <form action="welcome.php" method="post">
         Name: <input name = Name type="text" name="Enter Your Name"><br>
     <br>
     <br>
 
-
    <button type="submit" class="pure-button pure-button-primary" >Enter Raffle</button>
-</form>
+ <img src="/images/EnterToWin.png" alt="Enter to Win" style="width:304px;height:228px;">
+		     
+		</form>
 							
 </div
 	<?php include('includes/footer.php') ?>
