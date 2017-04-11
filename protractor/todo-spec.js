f5d998e56888f3966bc924f4b9c20e3bd4233304
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-11 10:26:19 +0100

    Update Test with new button

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -3,6 +3,6 @@ describe('CA Raffle UI Test', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://preprod.cacdsolutions.com/index.php');
     element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Enter Raffle')).click();
+    element(by.buttonText('Enter your Name and click here to Enter')).click();
     });
 });
