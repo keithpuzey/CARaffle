c65e08afe83b18aa9af3155a8fe554cbe4f75746
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-11 10:32:33 +0100

    Update

--- includes/adminheader.php
+++ includes/adminheader.php
@@ -1,19 +1,30 @@
-<div id="layout" class="pure-g">
-		<div class="sidebar pure-u-1 pure-u-md-1-4">
-			<div class="header">
-				<h1 class="brand-title">CA Kick Off</h1>
-				<h2 class="brand-tagline">Enter to Win</h2>
-				<h2 class="brand-tagline">Build Number $BUILD$ </h2>
 
+	<div id="layout" class="pure-g">
+	
+	<div class="sidebar pure-u-1 pure-u-md-1-4">
+	<IMG SRC="/images/raffle.gif" ALT="Raffle" WIDTH=280 HEIGHT=200>
+		<br>
+		<br>
+		<br>
+			<h1 class="brand-tagline">CA Kick Off</h1>
+		
+			<br>
+		        <h1 class="brand-tagline">Raffle Draw</h1>
+			<br>
+				<br>
+				<h2 class="brand-tagline">Build Number $BUILD$ </h2>
+    <br>
 				<nav class="nav">
 					<ul class="nav-list">
 						<li class="nav-item">
 							<a class="pure-button" href="index2.php">Home</a>
 						</li>
 						<li class="nav-item">
-							<a class="pure-button" href="about2.php">About</a>
+							<a class="pure-button" href="about2.php">Rules</a>
 						</li>
 					</ul>
+					
+			
 				</nav>
 			</div>
 		</div>
