b28268fb853b953cd6619ca9174a98c0c48fce9d
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-17 12:02:06 -0700

    Include Session

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php session_start() ; 
+            foreach ($_SESSION as $key => $value) 
+            {
+                //$line[$i] = $_SESSION[$key] = null;
+                unset($_SESSION[$key]); // - will wipe out the refs totally.
+            }
+			
+			$_SESSION['orderConfirmFinalised'] = false;
+?>
 
 <html>
 
