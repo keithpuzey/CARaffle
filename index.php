08244f225ff7e9172010c7078b17efe33752d8eb
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-17 09:55:28 -0700

    Include AXA

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
 		<link rel="stylesheet" href="css/layouts/store.css">
 		<title>CA Raffle</title>
-
+		<script type="text/javascript" id="ca_eum_ba" agent=browser src="https://axa.trials.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector.axa.trials.ca.com/profiles/PUZKE01%40CA.COM-C/CA%20Raffle?agent=browser" data-tenantID="PUZKE01@CA.COM-C" data-appID="CA Raffle" data-appKey="6d832950-238e-11e7-bee2-a9dcb512d6bd" ></script>
 	</head>
 	<body>
 	<?php include('includes/header.php') ?>
