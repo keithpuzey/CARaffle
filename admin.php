33d4ea4d8f80ad9bf10e76fb2cb8159c801afb8d
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-18 06:59:37 -0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -32,8 +32,8 @@ $lines = file($myFile,FILE_IGNORE_NEW_LINES);//file in to an array
                         
    <font size="10" color="blue"><p class="blink"><?=$lines[5]?> </p></font>
      
-                           <font size="20" color="blue"><p class="blink"><?=$lines[3]?> </p></font>
-                           <font size="15" color="blue"><p class="blink"><?=$lines[2]?> </p></font>
+                           <font size="10" color="blue"><p class="blink"><?=$lines[3]?> </p></font>
+                           <font size="10" color="blue"><p class="blink"><?=$lines[2]?> </p></font>
            
 
 
