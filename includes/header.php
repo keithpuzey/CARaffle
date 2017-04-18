79c6ef5df3237e9ca638eee7de5d91f8594b6fa0
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-17 21:22:30 -0700

    center

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@
 				<nav class="nav">
 					<ul class="nav-list">
 						<li class="nav-item">
-					<center>		<a class="pure-button" href="index.php">Refresh</a></center>
+					<center><a class="pure-button" href="index.php">Refresh</a></center>
 						</li>
 
 					</ul>
