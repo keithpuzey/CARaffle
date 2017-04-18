74481bd73ba75d786c29567831de1549ca369f44
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-18 09:05:05 -0700

    Update index.php

--- index.php
+++ index.php
@@ -22,8 +22,9 @@
 	<br>
 	<br>
 
-		<font size="10" color="blue">Enter the Raffle here  -  http://cdbu.io</font>
+	
 	<div class="content pure-u-1 pure-u-md-3-4">
+		<font size="10" color="blue">Enter the Raffle here  -  http://cdbu.io</font>
 	
 	<img src="/images/enter-to-win.gif" alt="Enter to Win" style="width:304px;height:228px;">			
 	<br>
