8826f046835d6ca4c5b8a9e3c5e7581fc54d24c9
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-17 22:44:01 -0700

    Center and new graphic

--- includes/adminheader.php
+++ includes/adminheader.php
@@ -1,25 +1,25 @@
-<div id="layout" class="pure-g">
+<div id="layout" class="pure-g" align="center">
 	
 	<div class="sidebar pure-u-1 pure-u-md-1-4">
-	<IMG SRC="/images/raffle.gif" ALT="Raffle" WIDTH=280 HEIGHT=200>
+	<IMG SRC="/images/raffle-small.gif" ALT="Raffle" WIDTH=280 HEIGHT=200 >
 		<br>
 		<br>
 		<br>
-			<h1 class="brand-tagline">Continuous Delivery</h1>
-					<h1 class="brand-tagline">Business Unit</h1>
-					<h1 class="brand-tagline">Raffle</h1>
+			<center><h1 class="brand-tagline">Continuous Delivery</h1></center>
+					<center><h1 class="brand-tagline">Business Unit</h1></center>
+					<center><h1 class="brand-tagline">Raffle</h1></center>
 				<br>
 				<br>
 				<br>
-				<h2 class="brand-tagline">Build Number $BUILD$ </h2>
+				<center><h2 class="brand-tagline">Build Number $BUILD$ </h2></center>
     <br>
 				<nav class="nav">
 					<ul class="nav-list">
 						<li class="nav-item">
-							<a class="pure-button" href="index.php">Refresh</a>
+					<a class="pure-button" href="index.php">Refresh</a>
 						</li>
-
+						
 					</ul>
 				</nav>
-			</div>
+		</div>
 		</div>
