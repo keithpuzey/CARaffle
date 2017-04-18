0f30e615e139ca1d6d4254f77ac8758e9e6161c5
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-17 21:20:30 -0700

    centre text and change graphic

--- includes/header.php
+++ includes/header.php
@@ -5,18 +5,18 @@
 		<br>
 		<br>
 		<br>
-			<h1 class="brand-tagline">Continuous Delivery</h1>
-					<h1 class="brand-tagline">Business Unit</h1>
-					<h1 class="brand-tagline">Raffle</h1>
+			<center><h1 class="brand-tagline">Continuous Delivery</h1></center>
+					<center><h1 class="brand-tagline">Business Unit</h1></center>
+					<center><h1 class="brand-tagline">Raffle</h1></center>
 				<br>
 				<br>
 				<br>
-				<h2 class="brand-tagline">Build Number $BUILD$ </h2>
+				<center><h2 class="brand-tagline">Build Number $BUILD$ </h2></center>
     <br>
 				<nav class="nav">
 					<ul class="nav-list">
 						<li class="nav-item">
-							<a class="pure-button" href="index.php">Refresh</a>
+					<center>		<a class="pure-button" href="index.php">Refresh</a></center>
 						</li>
 
 					</ul>
