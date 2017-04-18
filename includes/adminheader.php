f049a5d7dcf97d1a13b9512684c8d0a056727266
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-17 21:18:59 -0700

    change label

--- includes/adminheader.php
+++ includes/adminheader.php
@@ -1,30 +1,25 @@
-
-	<div id="layout" class="pure-g">
+<div id="layout" class="pure-g">
 	
 	<div class="sidebar pure-u-1 pure-u-md-1-4">
 	<IMG SRC="/images/raffle.gif" ALT="Raffle" WIDTH=280 HEIGHT=200>
 		<br>
 		<br>
 		<br>
-			<h1 class="brand-tagline">CA Kick Off</h1>
-		
-			<br>
-		        <h1 class="brand-tagline">Raffle Draw</h1>
-			<br>
+			<h1 class="brand-tagline">Continuous Delivery</h1>
+					<h1 class="brand-tagline">Business Unit</h1>
+					<h1 class="brand-tagline">Raffle</h1>
+				<br>
+				<br>
 				<br>
 				<h2 class="brand-tagline">Build Number $BUILD$ </h2>
     <br>
 				<nav class="nav">
 					<ul class="nav-list">
 						<li class="nav-item">
-							<a class="pure-button" href="index2.php">Home</a>
+							<a class="pure-button" href="index.php">Refresh</a>
 						</li>
-						<li class="nav-item">
-							<a class="pure-button" href="about2.php">Rules</a>
-						</li>
+
 					</ul>
-					
-			
 				</nav>
 			</div>
 		</div>
