f9c1ea765ff25a321a432b71671d9dc877ab06d6
keithpuzey <keithpuzey@users.noreply.github.com>
2017-04-21 07:31:01 -0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@
                 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
                 <link rel="stylesheet" href="css/layouts/store.css">
                 <title>Congratulations</title>
-  <script type="text/javascript" id="ca_eum_ba" agent=browser src="https://axa.trials.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector.axa.trials.ca.com/profiles/PUZKE01%40CA.COM-C/CA%20Raffle?agent=browser" data-tenantID="PUZKE01@CA.COM-C" data-appID="CA Raffle" data-appKey="6d832950-238e-11e7-bee2-a9dcb512d6bd" ></script>
+  <script type="text/javascript" id="ca_eum_ba" agent=browser src="https://axa.saas.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector.axa.saas.ca.com/api/1/urn:ca:tenantId:48B428C9-C483-B954-105A-A612C70885A2/urn:ca:appId:Raffle%20Application/profile?agent=browser" data-tenantID="48B428C9-C483-B954-105A-A612C70885A2" data-appID="Raffle Application" data-appKey="fa732d70-269e-11e7-b5cd-994429ed59ce" ></script>
                
         </head>
         <body>
