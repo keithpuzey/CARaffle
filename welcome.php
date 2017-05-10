397d3426e9630b99f5a199e8c9ca790157c2616d
keithpuzey <keithpuzey@users.noreply.github.com>
2017-05-10 08:04:56 +0100

    User Validation update
    Check for duplicate raffle entries and display appropriate message when a duplicate is detected

--- welcome.php
+++ welcome.php
@@ -4,9 +4,8 @@
                 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
                 <link rel="stylesheet" href="css/layouts/store.css">
                 <title>Congratulations</title>
-                <script type="text/javascript" id="ca_eum_ba_ext" src="https://axa.saas.ca.com/mdo/v1/sdks/browser/BAExt.js"></script>
-  <script type="text/javascript" id="ca_eum_ba" agent=browser src="https://axa.saas.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector.axa.saas.ca.com/api/1/urn:ca:tenantId:48B428C9-C483-B954-105A-A612C70885A2/urn:ca:appId:Raffle%20Application/profile?agent=browser" data-tenantID="48B428C9-C483-B954-105A-A612C70885A2" data-appID="Raffle Application" data-appKey="fa732d70-269e-11e7-b5cd-994429ed59ce" ></script>
-               
+                 <script type="text/javascript" id="ca_eum_ba" agent=browser src="https://axa.saas.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector.axa.saas.ca.com/api/1/urn:ca:tenantId:48B428C9-C483-B954-105A-A612C70885A2/urn:ca:appId:Raffle%20Application/profile?agent=browser" data-tenantID="48B428C9-C483-B954-105A-A612C70885A2" data-appID="Raffle Application" data-appKey="fa732d70-269e-11e7-b5cd-994429ed59ce" async></script>
+         
         </head>
         <body>
         <?php include('includes/header.php') ?>
@@ -15,13 +14,20 @@
 
 if(isset($_POST['Name'])&&!empty($_POST['Name'])) {
     $data = $_POST['Name'] . "\n";
+ 
+    $fileinput = file_get_contents('/var/www/html/mydata.txt');
+    if (stripos($fileinput, $data) !== false)
+    {
+    }
+else
+   
     $ret = file_put_contents('/var/www/html/mydata.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret == false) {
-        $Success = 'There was an error, please contact your administrator';
+        $Success = 'has already been entered for the raffle, please use your full name and remember only one entry per raffle is allowed';
     }
     else {
 
-       $Success = 'Congratulations your entry for the raffle has been accepted';
+       $Success = ' - your entry for the raffle has been accepted';
     }
 }
 else {
@@ -34,11 +40,10 @@ else {
    
         <br>
 
-<p>   <b> <?=$Success?> </b> </p>
+<p>   <b> <?=$data?> <?=$Success?> </b> </p>
 
 
 <?php include('includes/footer.php') ?>
 
 </body>
 </html>
-
