1f263829960b47bc3e0278f94b4a594a1111073f
dmitry.orlov <dmitry.orlov@ca.com>
2017-06-01 22:10:30 +0300

    Initial app structure.

--- axa.php
+++ axa.php
@@ -7,7 +7,7 @@
         <body>
  
      <div class="content pure-u-1 pure-u-md-3-4">
-  			<IMG SRC="/images/AXA.png" ALT="CA AXA" height="100%" width="100%">
+  			<IMG SRC="images/AXA.png" ALT="CA AXA" height="100%" width="100%">
              
         <br>
 
--- saucelabs.php
+++ saucelabs.php
@@ -7,7 +7,7 @@
         <body>
 
      <div class="content pure-u-1 pure-u-md-3-4">
-  			<IMG SRC="/images/Saucelabs.png" ALT="Saucelabs" height="100%" width="100%">
+  			<IMG SRC="images/Saucelabs.png" ALT="Saucelabs" height="100%" width="100%">
              
         <br>
 
