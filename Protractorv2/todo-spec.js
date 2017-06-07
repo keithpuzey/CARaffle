ce0ae52dacee45608077f30ca779472493274b73
keithpuzey <keithpuzey@users.noreply.github.com>
2017-06-07 20:36:33 +0100

    Update todo-spec.js

--- Protractorv2/todo-spec.js
+++ Protractorv2/todo-spec.js
@@ -3,7 +3,7 @@ describe('CA Raffle UI Test - emea Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://emea.cdbu.io');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Submit')).click();
     });
 });
