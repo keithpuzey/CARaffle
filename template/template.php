412e7abe48a85238f7b5adf3de9430f12380622e
dmitry.orlov <dmitry.orlov@ca.com>
2017-06-07 20:16:48 +0300

    axa snippet

--- template/template.php
+++ template/template.php
@@ -16,6 +16,8 @@
     <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css">
     <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css">
 
+    <?php $this->axaSnippet(); ?>
+
     <style type="text/css">
         * { box-sizing: border-box; }
 
