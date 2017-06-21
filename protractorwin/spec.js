5072705b2b93f53ac5a663ed6000d1ff6a36f2a8
keithpuzey <keithpuzey@users.noreply.github.com>
2017-06-21 09:08:02 +0100

    Update spec.js

--- protractorwin/spec.js
+++ protractorwin/spec.js
@@ -1,4 +1,4 @@
-describe('CA Raffle UI Test - Win Environment', function(){
+describe('CA MSF Raffle UI Test - Win Environment', function(){
   it('Run tests Against CA Raffle site (Win)', function(){
   }); 
 });
