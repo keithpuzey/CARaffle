df0e4cb492c5c90a3db55ebdf73105164cbf4765
keithpuzey <keithpuzey@users.noreply.github.com>
2017-06-21 07:48:25 +0100

    Update todo-spec.js

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -1,8 +1,8 @@
-describe('CA Raffle UI Test', function() {
+describe('CA MSF Raffle UI Test', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://preprod.cacdsolutions.com/index.php');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
+    element(by.buttonText('I\'m In!')).click();
     });
 });
