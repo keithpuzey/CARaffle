ec5db1444a092b3a1a12522b7ecf1ab63a8ec6a6
keithpuzey <keithpuzey@users.noreply.github.com>
2017-06-21 10:04:29 +0100

    Update todo-spec.js

--- protractorraffle/todo-spec.js
+++ protractorraffle/todo-spec.js
@@ -2,7 +2,7 @@ describe('CA Raffle UI Test - Raffle Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://raffle-preprod.us-east-1.elasticbeanstalk.com/index.php');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
+    element(by.buttonText('I\'m In!')).click();
     });
 });
