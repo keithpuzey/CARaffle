f810df8da55a0dc6e1ae3a8181dd03212828a778
keithpuzey <keithpuzey@users.noreply.github.com>
2017-08-15 09:27:37 +0100

    Update todo-spec.js

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -2,7 +2,7 @@ describe('CA MSF CDBU Raffle UI Test', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://CARaffle3.us-east-1.elasticbeanstalk.com');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
+    element(by.buttonText('I\'m In!')).click();
     });
 });
