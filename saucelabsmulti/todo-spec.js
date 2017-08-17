1dc45f35f8af88d5e989bd3df04ebb2259a13294
keithpuzey <keithpuzey@users.noreply.github.com>
2017-08-17 17:54:58 +0100

    Update todo-spec.js

--- saucelabsmulti/todo-spec.js
+++ saucelabsmulti/todo-spec.js
@@ -1,7 +1,7 @@
 describe('The Modern Software Factory Demo UI Test - CDBU.io', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://naeast-preprod.cdbu.io');
+    browser.get('http://preprod.cdbu.io');
     element(by.css('[name="name"]')).sendKeys('Selenium Test User ' + parseInt(Math.random() * (10000)));
     element(by.buttonText('I\'m In!')).click();
     const EC = protractor.ExpectedConditions;
