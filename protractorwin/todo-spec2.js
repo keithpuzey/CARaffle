3c656c8e2a81638b9d22574245ccb1a7e27918d8
keithpuzey <keithpuzey@users.noreply.github.com>
2017-10-29 18:55:06 +0000

    Update todo-spec2.js

--- protractorwin/todo-spec2.js
+++ protractorwin/todo-spec2.js
@@ -1,1 +1,9 @@
 
+describe('CA MSF Raffle UI Test - Win Environment', function() {
+  it('should add a todo', function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://win-preprod.us-east-1.elasticbeanstalk.com/index.php');
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User2');
+    element(by.buttonText('I\'m In!')).click();
+    });
+});
